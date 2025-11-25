c01dde650372f6e0898192bc04877b5dba24cfdc
bmbhi15 <admin@Admins-MacBook-Pro.local>
2025-11-25 15:33:00 +0530

    Changes project structure from js to ts. Added Navbar. Removed incorrect assets. Added Dayjs library

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <div>
-      <p>Portfolio Project</p>
-    </div>
+    <main>
+      <NavBar />
+    </main>
   );
 }
