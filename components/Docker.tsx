a73d8580c0e6d56e81f49595178c11a8692e2286
bmbhi15 <admin@Admins-MacBook-Pro.local>
2025-11-26 14:20:53 +0530

    added UI for dock

--- components/Docker.tsx
+++ components/Docker.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { dockApps } from "@/lib/constants";
 
 const Docker = () => {
-  return <div>Docker</div>;
+  return (
+    <section id="dock">
+      <div className="dock-container">
+        <ul className="flex flex-row space-x-5">
+          {dockApps.map((dock) => (
+            <li key={dock.id} className="dock-icon">
+              <button>
+                <img src={`/images/${dock.icon}`} alt={dock.name} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Docker;
