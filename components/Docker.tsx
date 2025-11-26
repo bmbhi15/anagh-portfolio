4c184f9fd01c63646e42176c5fe4742f13b44c12
bmbhi15 <admin@Admins-MacBook-Pro.local>
2025-11-26 14:24:43 +0530

    Added tooltip to all apps in the dock

--- components/Docker.tsx
+++ components/Docker.tsx
@@ -1,20 +1,26 @@
+"use client";
 import React from "react";
 import { dockApps } from "@/lib/constants";
+import { Tooltip } from "react-tooltip";
 
 const Docker = () => {
   return (
     <section id="dock">
       <div className="dock-container">
         <ul className="flex flex-row space-x-5">
-          {dockApps.map((dock) => (
-            <li key={dock.id} className="dock-icon">
-              <button>
-                <img src={`/images/${dock.icon}`} alt={dock.name} />
+          {dockApps.map((app) => (
+            <li key={app.id} className="dock-icon">
+              <button
+                data-tooltip-id="dock-tooltip"
+                data-tooltip-content={app.name}
+              >
+                <img src={`/images/${app.icon}`} alt={app.name} />
               </button>
             </li>
           ))}
         </ul>
       </div>
+      <Tooltip id="dock-tooltip" />
     </section>
   );
 };
