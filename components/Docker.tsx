585b8024224273d5fc89fda29e67d3f435b89fc9
bmbhi15 <admin@Admins-MacBook-Pro.local>
2025-11-26 15:28:08 +0530

    Completed Dock with full animation

--- components/Docker.tsx
+++ components/Docker.tsx
@@ -1,18 +1,102 @@
 "use client";
-import React from "react";
+import { RefObject, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { dockApps } from "@/lib/constants";
 import { Tooltip } from "react-tooltip";
 
+const HEIGHT = {
+  min_h: 0,
+  max_h: -50,
+};
+const SCALE = {
+  min_s: 1,
+  max_s: 1.7,
+};
+const calculateIntensity = (
+  e: MouseEvent,
+  leftContainer: number,
+  elem: HTMLButtonElement
+) => {
+  const { left, width } = elem.getBoundingClientRect();
+  const mouseX = e.clientX - leftContainer;
+  const elemX = left - leftContainer + width / 2;
+  const distance = mouseX - elemX;
+  const intensity = Math.exp(-(distance ** 2) / 10000);
+
+  return intensity;
+};
+
+const addEventListenerToDock = (
+  containerRef: RefObject<HTMLUListElement | null>
+) => {
+  if (!containerRef.current) return;
+  const { left } = containerRef.current.getBoundingClientRect();
+
+  const handleMouseMove = (e: MouseEvent) => {
+    const iconElements = containerRef.current?.querySelectorAll("button");
+
+    if (!iconElements) return;
+    iconElements.forEach((elem) => {
+      const { max_h, min_h } = HEIGHT;
+      const { max_s, min_s } = SCALE;
+      const intensity = calculateIntensity(e, left, elem);
+      const idSelector = `#${elem.id}`;
+      console.log(elem);
+      gsap.to(idSelector, {
+        y: min_h + (max_h - min_h) * intensity,
+        scale: min_s + (max_s - min_s) * intensity,
+        duration: 0.5,
+      });
+    });
+  };
+
+  const handleMouseLeave = () => {
+    const iconElements = containerRef.current?.querySelectorAll("button");
+
+    if (!iconElements) return;
+    iconElements.forEach((elem, id) => {
+      const { min_h } = HEIGHT;
+      const idSelector = `#${elem.id}`;
+      gsap.to(idSelector, {
+        y: 0,
+        scale: 1,
+        duration: 0.5,
+      });
+    });
+  };
+
+  containerRef?.current.addEventListener("mousemove", handleMouseMove);
+  containerRef?.current.addEventListener("mouseleave", handleMouseLeave);
+
+  return () => {
+    containerRef.current?.removeEventListener("mousemove", handleMouseMove);
+    containerRef.current?.removeEventListener("mouseleave", handleMouseLeave);
+  };
+};
+
 const Docker = () => {
+  const containerRef = useRef<HTMLUListElement>(null);
+
+  useGSAP(() => {
+    const removeListener = addEventListenerToDock(containerRef);
+
+    return () => {
+      if (!removeListener) return;
+      removeListener();
+    };
+  }, []);
+
   return (
     <section id="dock">
       <div className="dock-container">
-        <ul className="flex flex-row space-x-5">
+        <ul ref={containerRef} className="flex flex-row space-x-5">
           {dockApps.map((app) => (
             <li key={app.id} className="dock-icon">
               <button
                 data-tooltip-id="dock-tooltip"
                 data-tooltip-content={app.name}
+                id={app.name}
               >
                 <img src={`/images/${app.icon}`} alt={app.name} />
               </button>
